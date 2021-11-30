069cde7152363e688c5c05f67e5b015b06ebcdca
vaibhaviprajapati <pvaibhavi.104@gmail.com>
2021-11-30 16:18:53 +0530

    add data

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
-const amount = 1
-if(amount < 10){
-    console.log('small number')
+const http = require('http');
 
-} else{
-    console.log('large number')
-}
+const server = http.createServer((req, res) => {
+    if(req.url === '/'){
+        res.write('welcome to our home page');
+    }
+    if(req.url === '/about'){
+    res.write('here is our short history');
+    }
+res.end(`<h1>Ooops!</h1>
+<p>we cant seem to find the page you are looking for</p>
+<a href="/">back home</a>
+`)
+})
 
-console.log(`hey it's my first node app!!`)
+server.listen(5000)
